1840bc070ef3962345bf35a59d608a236250a5ce
PC <you@example.com>
2022-06-25 14:36:49 +0100

    Updating Site Apperance

--- Js/JavaScript.js
+++ Js/JavaScript.js
@@ -1,19 +1,28 @@
 $(document).ready(function() {
-    // text
-    $('.text').animate({
-        marginTop: '-150px',
-        top: '300px',
-        opacity: '1'
-    });
-
     // logo
     $('main img').animate({
         opacity: '0.3'
     });
 
+    // Services
+    $(document).scroll(function(){
+        if(window.scrollY > 500) {
+            $('#ser > div > div.grid > div:nth-child(1) > i').addClass('animate__animated animate__bounce');
+            $('#ser > div > div.grid > div:nth-child(2) > i').addClass('animate__animated animate__bounce');
+            $('#ser > div > div.grid > div:nth-child(3) > i').addClass('animate__animated animate__bounce');
+            $('#ser > div > div.grid > div:nth-child(4) > i').addClass('animate__animated animate__bounce');
+            $('#ser > div > div.grid > div:nth-child(5) > i').addClass('animate__animated animate__bounce');
+            $('#ser > div > div.grid > div:nth-child(6) > i').addClass('animate__animated animate__bounce');
+        }
+    });
+
+    // btn
+    $('#con > div > div.holder > div:nth-child(2) > form > input.sub').click(function(){
+        $('#con > div > div.holder > div:nth-child(2) > form > input.sub').addClass('animate__animated animate__pulse');
+    });
+
     // Skills
     $(document).scroll(function(){
-        console.log(window.screenY);
         if(window.scrollY > 1200) {
             $('.hr1').css('width', '80%');
             $('.hr2').css('width', '90%');
