f026d99fe08e418681c95cbcc058f10e072e42dd
WalidMes <105548658+WalidMes@users.noreply.github.com>
2023-09-02 19:14:10 +0100

    added works section

--- Js/JavaScript.js
+++ Js/JavaScript.js
@@ -1,7 +1,7 @@
-$(document).ready(function() {
+$(document).ready(function () {
     // Services
-    $(document).scroll(function(){
-        if(window.scrollY > 500) {
+    $(document).scroll(function () {
+        if (window.scrollY > 500) {
             $('#ser > div > div.grid > div:nth-child(1) > i').addClass('animate__animated animate__bounce');
             $('#ser > div > div.grid > div:nth-child(2) > i').addClass('animate__animated animate__bounce');
             $('#ser > div > div.grid > div:nth-child(3) > i').addClass('animate__animated animate__bounce');
@@ -12,13 +12,13 @@ $(document).ready(function() {
     });
 
     // btn
-    $('#con > div > div.holder > div:nth-child(2) > form > input.sub').click(function(){
+    $('#con > div > div.holder > div:nth-child(2) > form > input.sub').click(function () {
         $('#con > div > div.holder > div:nth-child(2) > form > input.sub').addClass('animate__animated animate__pulse');
     });
 
     // Skills
-    $(document).scroll(function(){
-        if(window.scrollY > 1200) {
+    $(document).scroll(function () {
+        if (window.scrollY > 1200) {
             $('.hr1').css('width', '80%');
             $('.hr2').css('width', '90%');
             $('.hr3').css('width', '70%');
@@ -27,13 +27,31 @@ $(document).ready(function() {
         }
     });
 
-    $('.fa-bars').click(function(){
+    $('.fa-bars').click(function () {
         $('.nav').toggleClass('visible');
     });
 
-    $('main button').click(function (){
+    $('main button').click(function () {
         window.scrollTo({
             top: 2150,
         });
+    });
+
+    // Swipper
+    var swiper = new Swiper(".mySwiper", {
+        spaceBetween: 30,
+        centeredSlides: true,
+        autoplay: {
+            delay: 2500,
+            disableOnInteraction: false,
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
     });
 });
